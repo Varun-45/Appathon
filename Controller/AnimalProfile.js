96dc0a915af4ed4acef0123e033208c266fa3a17
unknown <varunmalpani.vm01@gmail.com>
2024-03-08 20:26:27 +0530

    added srch

--- Controller/AnimalProfile.js
+++ Controller/AnimalProfile.js
@@ -68,3 +68,20 @@ export const getAnimalProfilesByUserId = async (req, res) => {
     }
 };
 
+
+export const searchAnimalById = async (req, res) => {
+    try {
+        const { animalId } = req.body;
+
+        // Search for the animal by animalId
+        const animal = await Animal.findOne({ animalId });
+
+        if (!animal) {
+            return res.status(404).json({ message: 'Animal not found' });
+        }
+
+        res.status(200).json({ animal });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    }
+};
