4ac575248303cbe6b994fa0f11635ea88380ac08
unknown <varunmalpani.vm01@gmail.com>
2024-03-08 11:28:47 +0530

    adedd milkrecord

--- Controller/AnimalProfile.js
+++ Controller/AnimalProfile.js
@@ -83,3 +83,4 @@ export const getAnimalProfilesByUserId = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error', error: error.message });
     }
 };
+
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ import express from 'express';
 const routes = express.Router();
 import { saveUser, login } from '../Controller/Auth.js';
 import { createAnimalProfile, getAnimalProfilesByUserId } from '../Controller/AnimalProfile.js';
+import { createMilkProductionRecord } from '../Controller/MilkProduction.js'
 
 routes.post('/signup', saveUser);
 routes.post('/login', login);
 routes.post('/registerCattle', createAnimalProfile)
 routes.get('/getanimals', getAnimalProfilesByUserId)
+routes.post('/createRecord', createMilkProductionRecord)
 
 export default routes
